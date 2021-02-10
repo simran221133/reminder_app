76dd54576d78c4468e8628de1520ff025d9dde36
Simran <simran_kaur@manulife.com>
2021-02-09 21:55:01 -0500

    Updated pop-ups

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import time
+import tkinter as tk
+from tkinter import messagebox,simpledialog
 
 # ask user for a reminder
-print ("What should i remind you off?")
+# print ("What should i remind you off?")
+# import tkinter module to root
+ROOT = tk.Tk()
+ROOT.withdraw()
+message = simpledialog.askstring(title= "Reminder", prompt="What should i remind you off?")
 
 # take user's input
-message = str(input())
+# message = str(input())
 
 # ask user "in how many minutes"
-print ("In how many minutes?")
+# print ("In how many minutes?")
+
+ROOT.withdraw()
+local = simpledialog.askinteger(title= "Reminder", prompt="In how many minutes?")
 
 # convert the number of minutes to seconds
-local_time = float(input())
+# local_time = float(input())
 
 # converting minutes to seconds
-local_time = local_time * 60
+local = local * 60
 
 # number of minutes for which the program will sleep (from time module takes method sleep with variable local name)
-time.sleep(local_time)
+time.sleep(local)
 
 # after
-print(message)
+messagebox.showinfo("Notification",message)
