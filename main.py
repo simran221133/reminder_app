1f39aaae5faf7c943bd5ddc9ce2ca409b1582416
Simran <simran_kaur@manulife.com>
2021-02-10 22:09:44 -0500

    Updated all reminders to write in a csv file

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import time
+import csv
 import tkinter as tk
+from csv import writer
 from tkinter import messagebox,simpledialog
 
-# ask user for a reminder
-# print ("What should i remind you off?")
-# import tkinter module to root
+# import tkinter module to root window
 ROOT = tk.Tk()
+# Withdraw this widget from the screen such that it is unmapped and forgotten by the window manager
 ROOT.withdraw()
+# take string input from user in message with pop-up box to ask what to remind
 message = simpledialog.askstring(title= "Reminder", prompt="What should i remind you off?")
 
-# take user's input
-# message = str(input())
-
-# ask user "in how many minutes"
-# print ("In how many minutes?")
-
-ROOT.withdraw()
+# take integer input from user for after how many minutes
 local = simpledialog.askinteger(title= "Reminder", prompt="In how many minutes?")
 
 # convert the number of minutes to seconds
-# local_time = float(input())
-
 # converting minutes to seconds
 local = local * 60
 
+#open text file in append and write mode
+with open('reminder.txt', 'a+') as file_object:
+    file_object.seek(0) # point to the beginning of the file
+    data = file_object.read(100) # read for more objects in the file
+    if len(data) > 0 :
+        file_object.write("\n") # in next line
+        file_object.write(message + "," + str(local)) # concat into a text file with ","
+
+ROOT.withdraw()
+
 # number of minutes for which the program will sleep (from time module takes method sleep with variable local name)
-time.sleep(local)
+#time.sleep(local)
 
 # after
-messagebox.showinfo("Notification",message)
+#messagebox.showinfo("Notification",message)
